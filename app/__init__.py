beccbfb1bb4dd1ef5094abf5a950dfaf1d8351ec
Ivan Morett <luisivanmorett@gmail.com>
2021-05-18 13:18:09 -0500

    basic project structure

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+def execute():
+    print("hello_world")
---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from app import execute
+
+def run():
+  execute()
+  pass
+
+def test():
+  # TODO: run tests
+  pass
+
+def install():
+  # TODO: install requirements.txt
+  pass
+
+def prepare():
+  # TODO: prepare env
+  pass
+
+def init():
+  # TODO: source env/bin/activate
+  pass
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or sys.argv[1] == "run":
+        run()
+    elif sys.argv[1] == "test":
+        test()
+    elif sys.argv[1] == "install":
+        install()
+    elif sys.argv[1] == "prepare":
+        prepare()
+    elif sys.argv[1] == "init":
+        init()
+else:
+    print("Please run this script individually")
