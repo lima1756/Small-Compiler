417213e7210e84cfea8bbdbd3f373a03a968a963
Ivan Morett <luisivanmorett@gmail.com>
2021-05-28 13:58:50 -0500

    testing parse trees

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -12,12 +12,18 @@ class TestLex:
     def test_correct_input1(self):
         with open(os.path.join(TestLex.dir, "./input_tests/ok1.txt")) as f:
             tree = self.parser.parse(f.read())
+            ok_tree = open(os.path.join(
+                TestLex.dir, "./parse_trees/ok1_tree.txt")).read()
             assert len(self.parseManager.errors) == 0
+            assert str(tree) == ok_tree
 
     def test_correct_input2(self):
         with open(os.path.join(TestLex.dir, "./input_tests/ok2.txt")) as f:
             tree = self.parser.parse(f.read())
+            ok_tree = open(os.path.join(
+                TestLex.dir, "./parse_trees/ok2_tree.txt")).read()
             assert len(self.parseManager.errors) == 0
+            assert str(tree) == ok_tree
 
     def test_incorrect_input(self):
         with open(os.path.join(self.dir, "./input_tests/syntax_fail1.txt")) as f:
