fac3eb7c3d89ba5e28df38bfc336e977d52c8728
Ivan Morett <luisivanmorett@gmail.com>
2021-05-30 14:26:10 -0500

    testing tac with new inputs

--- tests/test_tac.py
+++ tests/test_tac.py
@@ -24,3 +24,19 @@ class TestTAC:
             expected_output = open(os.path.join(
                 TestTAC.dir, "./output_tests/ok2.out")).read()
             assert output == expected_output
+
+    def test_correct_input3(self):
+        with open(os.path.join(TestTAC.dir, "./input_tests/ok3.txt")) as f:
+            tree = self.parser.parse(f.read())
+            output = TACGenerator(tree).generate()
+            expected_output = open(os.path.join(
+                TestTAC.dir, "./output_tests/ok3.out")).read()
+            assert output == expected_output
+
+    def test_correct_input4(self):
+        with open(os.path.join(TestTAC.dir, "./input_tests/ok4.txt")) as f:
+            tree = self.parser.parse(f.read())
+            output = TACGenerator(tree).generate()
+            expected_output = open(os.path.join(
+                TestTAC.dir, "./output_tests/ok4.out")).read()
+            assert output == expected_output
